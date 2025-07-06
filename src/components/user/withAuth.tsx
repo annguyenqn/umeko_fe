e3f75c8c91854befd50f0b293027c6e7473f7d7e
AnNguyen <theesan2k1@gmail.com>
2025-07-06 21:31:28 +0700

    chore: test pipe;ine

--- src/components/user/withAuth.tsx
+++ src/components/user/withAuth.tsx
@@ -5,7 +5,6 @@ import { useRouter } from 'next/navigation'
 import { useAuthStore } from '@/store/useAuthStore'
 import type { ComponentType, FC } from 'react'
 
-// ✅ FIX: ràng buộc P mở rộng object thay vì IntrinsicAttributes
 export function withAuth<P extends Record<string, unknown>>(Component: ComponentType<P>): FC<P> {
     const ProtectedComponent: FC<P> = (props) => {
         const router = useRouter()
